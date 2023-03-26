ac6d9611c5c72cbce650aebea19a2aadb1cf7d97
Kiérico Souza <kiericograndao@gmail.com>
2023-03-25 20:49:07 -0400

    #3.6 - Componente: TextInput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,41 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@kierico-ui/react'
+
+/** configuração global do TextInput */
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+/** exportar uma variação do TextInput */
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'kierico.com/',
+  },
+}
